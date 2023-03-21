276bec3d324b80a2a6d0509c296e5ba71b796177
Peter Barnes <pe5erb@gmail.com>
2023-03-21 19:51:55 +0000

    tidying

--- config.h
+++ config.h
@@ -1,6 +1,3 @@
 #define MANAGER_AP "DoorLock"
-<<<<<<< HEAD
 #define www_username "admin"
 #define www_password "cooperative"
-=======
->>>>>>> c6d7a32 (let's not share passwords with the internet)
--- doorlock.ino
+++ doorlock.ino
@@ -17,7 +17,7 @@
 #include "pcb_roller_shutter.h"
 #include "logo.h"
 #include "config.h"
-#include "secrets.h"
+#include "secrets.h" // WiFi SSID and password
 
 /*-----( Declare objects )-----*/
 //Initialise display library with no reset pin by passing it -1
